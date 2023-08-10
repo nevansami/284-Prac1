7774cc5a74037269e4c235d88fe4bfff3578ac51
8xbbie <mogabudimatau@gmail.com>
2023-08-11 00:43:03 +0200

    Fixed nevan's task 3, i think

--- calculate_account.asm
+++ calculate_account.asm
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
-; This makes your function available to other files
 global calculate_account
 
 section .data
 ; ==========================
 ; Your data goes here
+
 ; ==========================
 
 section .text
@@ -19,38 +18,13 @@ calculate_account:
 ; Do not modify anything above this line unless you know what you are doing
 ; ==========================
 ; Your code goes here
-  mov eax, 0 ; This can be deleted, it just keeps function from causing a runtime error until completed
+  xor rax, rax
+  mov edx, 6468
+  mov eax, edx ; load 32bit integer pin
+  add rax, 10000 ; adding 10000 to pin for acc number
+  ;mov rax, rax ; moving acc number to rax
 ; ==========================
 ; Do not modify anything below this line unless you know what you are doing
 
   leave
-=======
-; This makes your function available to other files
-global calculate_account
-
-section .data
-; ==========================
-; Your data goes here
-; ==========================
-
-section .text
-; Inputs:
-;   rdi - pin
-; Outputs:
-;   eax - balance
-calculate_account:
-  push rbp
-  mov rbp, rsp
-
-; Do not modify anything above this line unless you know what you are doing
-; ==========================
-; Your code goes here
-  mov eax, dword [rdi] ; load 32bit integer pin
-  add eax, 10000 ; adding 10000 to pin for acc number
-  mov rax, rax ; moving acc number to rax
-; ==========================
-; Do not modify anything below this line unless you know what you are doing
-
-  leave
->>>>>>> 495cf75a87c267e283baee0e0c3d770e15a9f29b
   ret
