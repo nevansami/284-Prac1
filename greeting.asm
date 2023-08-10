1236ba5651e663f995330f43b7fdd1b6382bb433
Nevan Rahman <rahman.nevan@gmail.com>
2023-08-10 14:55:14 +0200

    First commit

---
+++ greeting.asm
@@ -0,0 +1,33 @@
+; This makes your function available to other files
+global greeting
+
+section .data
+; ==========================
+; Your data goes here
+  output db "Hello world", 0xa
+  length equ $ - output
+
+; ==========================
+
+section .text
+; void greeting()
+; This function prints a greeting to the screen
+greeting:
+  push rbp
+  mov rbp, rsp
+; Do not modify anything above this line unless you know what you are doing
+; ==========================
+; Your code goes here
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, output
+  mov edx, length
+  int 0x80
+
+  mov eax, 1
+  xor ebx, ebx
+  int 0x80
+; ==========================
+; Do not modify anything below this line unless you know what you are doing
+  leave
+  ret
