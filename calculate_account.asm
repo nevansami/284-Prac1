190cf500cfa0f6d677ddebb76249f44ef13ebb06
8xbbie <mogabudimatau@gmail.com>
2023-08-11 01:45:58 +0200

    Implemented task 4

--- calculate_account.asm
+++ calculate_account.asm
@@ -19,9 +19,12 @@ calculate_account:
 ; ==========================
 ; Your code goes here
   xor rax, rax
-  mov edx, 6468
+  mov edx, 6468 ;pretend as if this is the pin from user
   mov eax, edx ; load 32bit integer pin
   add rax, 10000 ; adding 10000 to pin for acc number
+
+  mov eax, 1
+  int 0x80
   ;mov rax, rax ; moving acc number to rax
 ; ==========================
 ; Do not modify anything below this line unless you know what you are doing
--- calculate_balance.asm
+++ calculate_balance.asm
@@ -4,6 +4,8 @@ global calculate_balance
 section .data
 ; ==========================
 ; Your data goes here
+section .bss
+  account resb 10
 ; ==========================
 
 section .text
@@ -16,11 +18,21 @@ section .text
 calculate_balance:
   push rbp
   mov rbp, rsp
-  
 ; Do not modify anything above this line unless you know what you are doing
 ; ==========================
 ; Your code goes here
-  mov eax, 0 ; This can be deleted, it just keeps function from causing a runtime error until completed
+  mov rdi, 16767
+  mov rsi, 6767 ;pretend as if the two registers have the passed in values. this is to be changed later
+  mov [account], rdi
+  mov rax, [account] ; store a copy of rdi in rax
+  add rdi, rsi ;A = A + P
+  imul rdi, rsi ;(A + P)*P
+  xor rax, rsi ; A = A xor P
+  and rax, rdi ; rax stores final result of computation
+
+
+  mov eax, 1 ; This can be deleted, it just keeps function from causing a runtime error until completed
+  int 0x80
 ; ==========================
 ; Do not modify anything below this line unless you know what you are doing
   leave
