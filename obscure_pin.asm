acafbb1b6f7b6da1ddd92b88dfe2d250c479e592
8xbbie <mogabudimatau@gmail.com>
2023-08-13 02:29:42 +0200

    Finished task 5

--- obscure_pin.asm
+++ obscure_pin.asm
@@ -3,6 +3,11 @@ global obscure_pin
 section .data
 ; ==========================
 ; Your data goes here
+  pin db "3647", 0
+  counter db 0
+
+section .bss
+  obscured resb 100
 ; ==========================
 
 ; void obscure_pin(char* pin)
@@ -16,8 +21,34 @@ obscure_pin:
 ; ==========================
 ; Your code goes here
   ; load PIN digits
+   ;;pretend the pin is already in register
+  mov ebx, 0 ;;counter variable
+  xor eax, eax
+  mov esi, pin
+;;this loop obscures the pin
+
+iterate :
+  movzx ecx, byte [esi]
+  cmp ecx, 0
+  je done
+  sub ecx, 48
+  xor ecx, 0xF
+  add ecx, 48
+  mov [obscured + ebx], ecx
+  mov eax, obscured
+  inc ebx
+  inc esi
+  jmp iterate
   ; obscure digits
   ; convert back to ASCII and store in reverse order
+done:
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, obscured
+  mov edx, 4
+  int 0x80
+  mov eax, 1
+  int 0x80
 ; ==========================
 ; Do not modify anything below this line unless you know what you are doing
 
